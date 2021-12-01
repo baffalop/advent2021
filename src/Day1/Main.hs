711f845b45d93b17d7e0763847acc6a80373583a
Nikita Gaidakov <ngaidakov@podfather.com>
2021-12-01 11:54:20 +0000

    Solve Day 1 part B

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,4 +1,7 @@
 module Day1.Main (parse, solveA, solveB) where
+
+import Data.List (tails)
+import Data.List.Extra (dropEnd)
     
 parse :: String -> [Int]
 parse = fmap read . lines
@@ -7,4 +10,7 @@ solveA :: [Int] -> Int
 solveA ns = length $ filter (< 0) $ zipWith (-) ns $ tail ns
 
 solveB :: [Int] -> Int
-solveB = undefined
+solveB = solveA . fmap sum . rollingWindows 3
+
+rollingWindows :: Int -> [a] -> [[a]]
+rollingWindows n = fmap (take n) . dropEnd (n - 1) . tails
