583d8c2b21f8cd8f308dd71f7f65449e0b669f81
Nikita Gaidakov <ngaidakov@podfather.com>
2021-12-02 14:04:27 +0000

    Solve Day 2 part A

--- src/Day2/Solution.hs
+++ src/Day2/Solution.hs
@@ -1,10 +1,35 @@
 module Day2.Solution (parse, solveA, solveB) where
-  
-parse :: String -> [(String, Int)]
-parse = undefined
 
-solveA :: [(String, Int)] -> (Int, Int)
-solveA = undefined
+import Data.List (foldl')
+import Text.Read (readMaybe)
+import Data.List.Extra (upper)
+import Data.Char (toUpper)
+import Data.Biapplicative ((<<*>>))
+import Data.Tuple.Extra (both)
+import Data.Maybe (fromMaybe)
 
-solveB :: [(String, Int)] -> (Int, Int)
+data Direction = Up | Down | Forward
+  deriving (Eq, Show, Read)
+
+parse :: String -> [(Direction, Int)]
+parse = fromMaybe (error "Parse failed") . traverse (parseWords . words) . lines
+
+parseWords :: [String] -> Maybe (Direction, Int)
+parseWords [dir, n] = (,) <$> readMaybe (titleCase dir) <*> readMaybe n
+parseWords _ = Nothing
+
+solveA :: [(Direction, Int)] -> Int
+solveA = uncurry (*) . foldr ((<<*>>) . both (+) . move) (0, 0)
+  where
+    move :: (Direction, Int) -> (Int, Int)
+    move (dir, n) = case dir of
+      Up -> (0, -n)
+      Down -> (0, n)
+      Forward -> (n, 0)
+
+solveB :: [(Direction, Int)] -> Int
 solveB = undefined
+
+titleCase :: String -> String
+titleCase "" = ""
+titleCase (first:rest) = toUpper first : rest
