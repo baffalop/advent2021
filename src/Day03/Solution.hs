de1ec593deb9f212377ea72b0f7b8dc1f9ff5fff
Nikita Gaidakov <ngaidakov@podfather.com>
2021-12-03 12:06:20 +0000

    I thought I'd solved day 3 part 2

--- src/Day03/Solution.hs
+++ src/Day03/Solution.hs
@@ -1,30 +1,45 @@
 module Day03.Solution (parse, solveA, solveB) where
 
-import Data.List (transpose, partition)
+import Data.Function (on)
+import Data.List (transpose, partition, maximumBy)
 import Control.Arrow ((&&&))
 import Data.Tuple.Extra (both)
 import Data.Maybe (mapMaybe)
 import Text.Read (readMaybe)
 
 type Binary = String
+type Bit = Char
 
-parse :: String -> Either Never [Binary]
+parse :: String -> Either a [Binary]
 parse = Right . lines
 
 solveA :: [Binary] -> Int
-solveA = uncurry (*) . both toInt . (id &&& invert) . fmap mostCommonDigit . transpose
+solveA = uncurry multiply . (id &&& fmap bitFlip) . fmap mostCommonBit . transpose
 
 solveB :: [Binary] -> Int
-solveB = undefined
+solveB input =
+  uncurry multiply $ both (\f -> matchByBitCriteria (f . mostCommonBit) input) (id, bitFlip)
 
-mostCommonDigit :: Binary -> Char
-mostCommonDigit ds =
+matchByBitCriteria :: (Binary -> Bit) -> [Binary] -> Binary
+matchByBitCriteria criteria input =
+  maximumBy (compare `on` (prefixMatch $ fmap criteria $ transpose input)) input
+
+prefixMatch :: Eq a => [a] -> [a] -> Int
+prefixMatch (x:xs) (y:ys) | x == y = 1 + prefixMatch xs ys
+prefixMatch _ _ = 0
+
+mostCommonBit :: Binary -> Bit
+mostCommonBit ds =
   if (length $ filter (== '0') ds) >= (length ds `div` 2)
     then '0' else '1'
+
+multiply :: Binary -> Binary -> Int
+multiply = (*) `on` toInt
 
 toInt :: Binary -> Int
 toInt =
   sum . zipWith (*) (fmap (2^) [0..]) . reverse . mapMaybe (readMaybe . (:[]))
 
-invert :: Binary -> Binary
-invert = fmap (\bit -> if bit == '0' then '1' else '0')
+bitFlip :: Bit -> Bit
+bitFlip '0' = '1'
+bitFlip _ = '0'
