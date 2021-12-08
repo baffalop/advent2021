b159755302b7c343a302567f8cc5f523fbbe8e43
Nikita Gaidakov <ngaidakov@podfather.com>
2021-12-08 12:16:22 +0000

    Refactor day 8 part 1 solution to use digit to positions map

--- src/Day08/Solution.hs
+++ src/Day08/Solution.hs
@@ -1,18 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Day08.Solution (parse, solveA, solveB) where
-  
+
+import Data.Maybe (mapMaybe)
 import Data.Text (Text)
 import Data.Attoparsec.Text (Parser)
+import Data.Map.Strict (Map)
+import Data.Set (Set)
 import qualified Data.Attoparsec.Text as P
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+import Data.Biapplicative (Bifunctor(second))
+import Data.Bifunctor (bimap)
 
 type Signal = String
 
 data Display = Display
-  { inputs :: [Signal] 
+  { inputs :: [Signal]
   , output :: [Signal]
   }
   deriving Show
+
+newtype Digit = Digit { fromDigit :: Int }
+  deriving (Eq, Ord)
 
 parse :: Text -> Either String [Display]
 parse = P.parseOnly $ display `P.sepBy1'` P.endOfLine
@@ -22,18 +32,40 @@ parse = P.parseOnly $ display `P.sepBy1'` P.endOfLine
       <$> signals
       <*  P.string " | "
       <*> signals
-    
+
     signals :: Parser [Signal]
     signals = signal `P.sepBy1'` P.char ' '
-    
+
     signal :: Parser Signal
     signal = P.many1' $ P.satisfy $ P.inClass "abcdefg"
-    
+
 solveA :: [Display] -> Int
-solveA = sum . fmap (length . filter isIdentifiableDigit . output)
-  where
-    isIdentifiableDigit :: Signal -> Bool
-    isIdentifiableDigit s = length s `elem` [2, 3, 4, 7]
+solveA = sum . fmap (length . mapMaybe obviously . output)
 
 solveB :: [Display] -> Int
 solveB = undefined
+
+obviously :: Signal -> Maybe Digit
+obviously s =
+  case Map.lookup (length s) segmentsDigits of
+    Just [d] -> Just d
+    _ -> Nothing
+
+segmentsDigits :: Map Int [Digit]
+segmentsDigits = flipMap $ Set.size <$> digitPositions
+
+digitPositions :: Map Digit (Set Char)
+digitPositions = Map.fromList $ bimap Digit Set.fromList <$>
+  [ (1, "ef")
+  , (2, "acdeg")
+  , (3, "acdfg")
+  , (4, "bcdf")
+  , (5, "abdfg")
+  , (6, "abdefg")
+  , (7, "acf")
+  , (8, "abcdefg")
+  , (9, "abcdfg")
+  ]
+
+flipMap :: Ord a => Map k a -> Map a [k]
+flipMap = Map.foldrWithKey (\k a -> Map.insertWith (<>) a [k]) Map.empty
