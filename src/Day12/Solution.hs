aa01e83dc0ac575bd571692c85bdb344d4ba1089
Nikita Gaidakov <ngaidakov@podfather.com>
2021-12-13 16:05:02 +0000

    Solve day 12 part 1

--- src/Day12/Solution.hs
+++ src/Day12/Solution.hs
@@ -1,14 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Day12.Solution (parse, solveA, solveB) where
-  
-import Data.Text (Text, unpack)
+module Day12.Solution where
+
+import Data.Text (Text)
 import Data.Attoparsec.Text (Parser)
 import qualified Data.Attoparsec.Text as P
 import qualified Data.Map as Map
 import qualified Data.Char as Char
+import Data.Maybe (fromMaybe)
+import Data.List (nub)
+import Data.Tuple.Extra (second, swap)
 
-type Map = Map.Map Cave [Cave]
+type CaveMap = Map.Map Cave [Cave]
 
 data Cave
   = Start
@@ -17,13 +20,13 @@ data Cave
   | End
   deriving (Eq, Ord, Show)
 
-parse :: Text -> Either String Map
+parse :: Text -> Either String CaveMap
 parse = P.parseOnly $
-  Map.fromListWith (<>) <$> tunnel `P.sepBy1'` P.endOfLine 
+  Map.fromListWith (nub ... (<>)) . prepare <$> link `P.sepBy1'` P.endOfLine
   where
-    tunnel :: Parser (Cave, [Cave])
-    tunnel = (,) <$> cave <* P.char '-' <*> ((:[]) <$> cave)
-    
+    link :: Parser (Cave, Cave)
+    link = (,) <$> cave <* P.char '-' <*> cave
+
     cave :: Parser Cave
     cave = P.choice
       [ Start <$ P.string "start"
@@ -31,9 +34,32 @@ parse = P.parseOnly $
       , Big <$> P.many1' (P.satisfy Char.isUpper)
       , Small <$> P.many1' (P.satisfy Char.isLower)
       ]
+    
+    prepare :: [(Cave, Cave)] -> [(Cave, [Cave])]
+    prepare = (<>) <$> fmap (second (:[])) <*> fmap (second (:[]) . swap)
 
-solveA :: Map -> Map
-solveA = id
+solveA :: CaveMap -> Int
+solveA = length . paths
 
-solveB :: Map -> Map
+paths :: CaveMap -> [[Cave]]
+paths map = explore [Start]
+  where
+    explore :: [Cave] -> [[Cave]]
+    explore [] = error "Empty path"
+    explore path@(current:prev) =
+      case current of
+        End -> [path]
+        Start | Start `elem` prev -> []
+        c@(Small _) | c `elem` prev -> []
+        c -> fromMaybe []
+          $ foldMap (explore . (: path))
+          <$> Map.lookup c map
+
+solveB :: CaveMap -> Int 
 solveB = undefined
+
+frequency :: Eq a => a -> [a] -> Int
+frequency x = length . filter (== x)
+
+(...) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
+(...) = (.) . (.)
